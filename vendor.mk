bd09adeddec4a1b09bd6b4e31ef6044e8faa7894
scorpio92 <scorpio92@mail.ru>
2015-02-15 12:25:59 +1000

    proprietary mk update

--- vendor.mk
+++ vendor.mk
@@ -1,5 +1,7 @@
-#Auidio
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/proprietary/etc/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/proprietary/etc/media_profiles.xml:system/etc/media_profiles.xml \
+    $(LOCAL_PATH)/proprietary/etc/vold.fstab:system/etc/vold.fstab \
     $(LOCAL_PATH)/proprietary/etc/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
     $(LOCAL_PATH)/proprietary/etc/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
     $(LOCAL_PATH)/proprietary/lib/egl/egl.cfg:system/lib/egl/egl.cfg \
@@ -12,3 +14,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/lib/libOpenVG.so:system/lib/libOpenVG.so \
     $(LOCAL_PATH)/proprietary/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so \
     $(LOCAL_PATH)/proprietary/lib/libmm-adspsvc.so:system/lib/libmm-adspsvc.so \
+    $(LOCAL_PATH)/proprietary/lib/libmmparser.so:system/lib/libmmparser.so \
+    $(LOCAL_PATH)/proprietary/lib/libqc-opt.so:system/lib/libqc-opt.so \
+    $(LOCAL_PATH)/proprietary/lib/libqdi.so:system/lib/libqdi.so \
+    $(LOCAL_PATH)/proprietary/lib/libqdp.so:system/lib/libqdp.so \
+    $(LOCAL_PATH)/proprietary/usr/idc/ft5x06_ts.idc:system/usr/idc/ft5x06_ts.idc \
+    $(LOCAL_PATH)/proprietary/ts.conf:system/ts.conf
